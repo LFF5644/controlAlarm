6674603a73a78ce3da46414387e861cd8729ab24
LFF <meerschweinchen5644@gmail.com>
2022-07-28 17:00:29 +0200

    working

---
+++ controlAlarm.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+const [node, file, ...args]=process.argv;
+const smarthomeAPI=require("tplink-smarthome-api");
+
+const addr="192.168.109.56";
+const HELP="Exampel: "+file+" [on|off]";
+
+async function main(){
+	console.log("Node: "+node);
+	console.log("Datei: "+file);
+	console.log("Argumente: "+JSON.stringify(args));
+	console.log("");
+
+	if(args[0]){
+		const client=new smarthomeAPI.Client();
+		const options={host:addr};
+		console.log("Trying to Open '"+addr+"'");
+		const device=await client.getDevice(options);
+		console.log("Finish!");
+		let turnOn=true;
+		if(args[0]=="1"){turnOn=true}
+		else if(args[0]=="on"){turnOn=true}
+		else if(args[0]=="an"){turnOn=true}
+		else if(args[0]=="online"){turnOn=true}
+
+		else if(args[0]=="0"){turnOn=false}
+		else if(args[0]=="off"){turnOn=false}
+		else if(args[0]=="aus"){turnOn=false}
+		else if(args[0]=="offline"){turnOn=false}
+		else{console.log(HELP);return(1)}
+		console.log("set power to "+(turnOn?"'ON'":"'OFF'"));
+		device.setPowerState(turnOn);
+		console.log("OK Finish!");
+
+		//console.log(device)
+	}else{
+		console.log(HELP)
+	}
+
+}
+main();
